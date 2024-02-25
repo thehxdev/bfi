db889456ff0d598fbace3a4f17325bd9dd6ff39b
thehxdev <xclimax1324@gmail.com>
2024-02-25 11:59:01 +0330

    changed main function structure for future changes to bfi

--- src/main.c
+++ src/main.c
@@ -2,23 +2,32 @@
 #include "bfi.h"
 
 
-#define BF_VERSION "2.3.0"
+#define BF_VERSION "2.4.0"
 
 
 int main(int argc, char *argv[]) {
+    int err = 0;
+    BF_State *bf = NULL;
+
     if (argc != 2) {
         /* Check command-line arguments */
         fprintf(stderr, "Usage: bfi <source-file>\n");
-        return 64;
+        err = 1;
+        goto exit;
     }
 
     /* Initialize BF_State and Deinit that at the
      * end of main function scope.
      * First command-line argument is BrainFuck
      * source code file path. */
-    BF_State DEINIT_BF_END *bf = bf_init(argv[1]);
+    bf = bf_init(argv[1]);
 
     /* BFI main loop. Execute tokens */
-    bf_execute(&bf->tl, &bf->arr);
-    return 0;
+    err = bf_execute(&bf->tl, &bf->arr);
+    if (err)
+        goto exit;
+
+exit:
+    bf_deinit(&bf);
+    return err;
 }
