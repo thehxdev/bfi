1da25c50cbf1a75e200f327c448f3167509e8f70
thehxdev <hossein.khosravi.ce@gmail.com>
2024-03-30 21:36:35 +0330

    updated execution of bfi

--- src/main.c
+++ src/main.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-/* Cap */
-#include "cap.h"
-
 /* BFI */
 #include "bfi.h"
 #include "log.h"
@@ -10,38 +7,27 @@
 #include "compiler.h"
 
 
-#define BFI_VERSION "2.7.6"
+#define BFI_VERSION "3.0.0"
 
 void usage(void);
+
 
 int main(int argc, char *argv[]) {
     int err = 0, run = 0, x64asm = 0;
     char *src = NULL, *out = NULL;
     BF_State *bf = NULL;
-    Cap_t *cap = cap_init(argc, argv);
 
-    (void)cap_register_flag(cap, NULL, "r", "Run a source file directly.");
-    (void)cap_register_flag(cap, NULL, "asm", "Compile a source file.");
-    (void)cap_register_flag(cap, NULL, "o", "Output file path to write compiler generated code.");
-
-    err = cap_parse_args(cap);
-    if (err) {
-        usage();
-        goto exit;
-    }
-
-
-    if ((src = cap_flag_getval(cap, NULL, "r"))) {
-        run  = 1;
-    } else if ((src = cap_flag_getval(cap, NULL, "asm"))) {
-        out = cap_flag_getval(cap, NULL, "o");
+    if (argc == 2) {
+        run = 1;
+    } else if (argc == 3) {
         x64asm = 1;
+        out = argv[2];
     } else {
-        BF_LOG_ERR("Invalid command-line arguments\n", NULL);
-        BF_LOG_ERR("Stopping bfi...\n", NULL);
         usage();
         goto exit;
     }
+    src = argv[1];
+
 
     bf = bf_init(src);
     if (!bf) {
@@ -51,15 +37,12 @@ int main(int argc, char *argv[]) {
     }
 
     if (run) {
-        if ((bf_init_data_array(&bf->arr)) == NULL)
-            goto exit;
-        err = bf_execute(&bf->tl, &bf->arr);
+        err = bf_execute(bf->tl);
     } else if (x64asm)
-        err = bf_compiler_x64gcc(&bf->tl, out);
+        err = bf_compiler_x64gcc(bf->tl, out);
 
 exit:
     bf_deinit(&bf);
-    cap_deinit(&cap);
     return err;
 }
 
@@ -67,9 +50,8 @@ exit:
 void usage(void) {
     fprintf(stderr,
             "BFI v" BFI_VERSION " - https://github.com/thehxdev/bfi\n"
-            "Cap v" CAP_VERSION " - https://github.com/thehxdev/cap\n"
             "Usage:\n"
-            "\tInterpreter: bfi -r <source-code-path>\n"
-            "\tx86 ASM Compiler: bfi -asm <source-code-path> -o <output-file-path>\n"
+            "\tInterpreter: bfi <source-code-path>\n"
+            "\tx86 ASM Compiler: bfi <source-code-path> <output-file-path>\n"
             );
 }
