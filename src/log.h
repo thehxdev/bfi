c63057f6117a9139eb9842d1346cc93340b68955
thehxdev <hossein.khosravi.ce@gmail.com>
2024-07-23 21:10:26 +0330

    enable logging only if SAFE_BFI is defined
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- src/log.h
+++ src/log.h
@@ -2,9 +2,13 @@
 #define BFI_LOG_H
 
 
-/* Log error messages */
-#define BF_LOG_ERR(format, ...) \
-    (void)fprintf(stderr, "[ERROR] %s: " format, __FUNCTION__, __VA_ARGS__)
+#ifdef SAFE_BFI
+    /* Log error messages */
+    #define BF_LOG_ERR(format, ...) \
+        (void)fprintf(stderr, "[ERROR] %s: " format, __FUNCTION__, __VA_ARGS__)
+#else
+    #define BF_LOG_ERR(format, ...)
+#endif
 
 
 #endif /* BFI_LOG_H */
